f0b3ea953c0ca91a9f36324a1051f952bd0fdbf7
Mochales Cappello <mochales.julen@uni.lan>
2021-04-27 12:08:19 +0200

    pruba comit actual

--- src/rubenteacher/Rubenteacher.java
+++ src/rubenteacher/Rubenteacher.java
@@ -16,6 +16,6 @@ public class Rubenteacher {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        System.out.println("esta prueba termina aqui.");}
     
 }
